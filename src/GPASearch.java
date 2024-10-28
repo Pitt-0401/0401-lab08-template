abf58ec4c05f47386d35f418b4981ee4d525b22c
Nadine von Frankenberg <vonfrankenberg@pitt.edu>
2024-10-28 11:17:45 -0400

    Add code template

---
+++ src/GPASearch.java
@@ -0,0 +1,16 @@
+/*
+ * Created on 2025-02-28
+ *
+ * Copyright (c) 2025 Nadine von Frankenberg
+ */
+
+public class GPASearch {
+
+    public static void search(Student[] array, double threshold, boolean isAbove) {
+        // TODO: Task 2 - Implement a search algorithm that returns an array of students above or below a given GPA
+        
+        return; // TODO: determine return type (indices or students)
+    }
+
+    // Hint: Define a method that returns the number of students that match the filter
+}
